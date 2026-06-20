ca65de898b20dddd44af7080a00ddeae1ee6e578
Joaquin Van Messem <vanmessemjoaquin@gmail.com>
2026-06-19 21:19:00 -0300

    Middleware y Health-Check

--- Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15.Api/Program.cs
@@ -1,32 +1,40 @@
 
-namespace Dsw2026Ej15
+using Dsw2026Ej15.Api.Middlewares;
+using Dsw2026Ej15.Data.Implementations;
+using Dsw2026Ej15.Domain.Interfaces;
+
+namespace Dsw2026Ej15;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
+
+        builder.Services.AddHealthChecks();
+
+        var app = builder.Build();
+
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.MapHealthChecks("/health-check");
+
+        app.Run();
+
     }
 }
--- Dsw2026Ej15.Domain/Exceptions/ValidationException.cs
+++ Dsw2026Ej15.Domain/Exceptions/ValidationException.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Dsw2026Ej15.Domain.Exceptions
 {
     public class ValidationException : Exception
     {
-        public ValidationException(String messege) : base(messege)
+        public IReadOnlyList<string> Errors { get; }
+
+        public ValidationException(string message) : base(message)
         {
+            Errors = new List<string> { message };
+        }
+
+        public ValidationException(IEnumerable<string> errors) : base("Se encontraron errores de validación")
+        {
+            Errors = new List<string>(errors);
         }
     }
 }
